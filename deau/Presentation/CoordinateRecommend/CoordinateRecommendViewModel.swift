9f216d00ca9764fe77a7a2c0cfcffcc48379dfc6
hamadayuuki <yuukirinnma@gmail.com>
2024-11-27 06:25:00 +0900

    Add coordinate image

--- deau/Presentation/CoordinateRecommend/CoordinateRecommendViewModel.swift
+++ deau/Presentation/CoordinateRecommend/CoordinateRecommendViewModel.swift
@@ -9,12 +9,12 @@ import Observation
 @Observable @MainActor
 final class CoordinateRecommendViewModel {
     private(set) var coordinates: [Coordinate] = [
-        .init(imageName: "coordinate01", title: "クリスマスに映える服", time: "21時間前", userIconName: "userIcon01", userName: "だーはま", userHeight: "199cm", isFavorite: false),
-        .init(imageName: "coordinate02", title: "クリスマスに映える服", time: "21時間前", userIconName: "userIcon02", userName: "だーはま", userHeight: "199cm", isFavorite: true),
-        .init(imageName: "coordinate03", title: "クリスマスに映える服", time: "21時間前", userIconName: "userIcon01", userName: "だーはま", userHeight: "199cm", isFavorite: true),
-        .init(imageName: "coordinate04", title: "クリスマスに映える服", time: "21時間前", userIconName: "userIcon02", userName: "だーはま", userHeight: "199cm", isFavorite: false),
-        .init(imageName: "coordinate05", title: "クリスマスに映える服", time: "21時間前", userIconName: "userIcon01", userName: "だーはま", userHeight: "199cm", isFavorite: false),
-        .init(imageName: "coordinate06", title: "クリスマスに映える服", time: "21時間前", userIconName: "userIcon02", userName: "だーはま", userHeight: "199cm", isFavorite: true),
+        .init(imageName: "coordinate02", title: "クリスマスに映える服", time: "21時間前", userIconName: "userIcon01", userName: "だーはま", userHeight: "199cm", isFavorite: false),
+        .init(imageName: "coordinate10", title: "クリスマスに映える服", time: "21時間前", userIconName: "userIcon02", userName: "だーはま", userHeight: "199cm", isFavorite: true),
+        .init(imageName: "coordinate16", title: "クリスマスに映える服", time: "21時間前", userIconName: "userIcon01", userName: "だーはま", userHeight: "199cm", isFavorite: true),
+        .init(imageName: "coordinate20", title: "クリスマスに映える服", time: "21時間前", userIconName: "userIcon02", userName: "だーはま", userHeight: "199cm", isFavorite: false),
+        .init(imageName: "coordinate08", title: "クリスマスに映える服", time: "21時間前", userIconName: "userIcon01", userName: "だーはま", userHeight: "199cm", isFavorite: false),
+        .init(imageName: "coordinate19", title: "クリスマスに映える服", time: "21時間前", userIconName: "userIcon02", userName: "だーはま", userHeight: "199cm", isFavorite: true),
     ]
 
     func favoriteButtonTapped(index: Int) {
