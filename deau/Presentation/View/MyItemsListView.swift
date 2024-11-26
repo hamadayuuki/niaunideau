0d7196f2b6ceb775066a28e7adcd576ac7105af1
hamadayuuki <yuukirinnma@gmail.com>
2024-11-26 13:37:43 +0900

    Init project

---
+++ deau/Presentation/View/MyItemsListView.swift
@@ -0,0 +1,18 @@
+//
+//  MyItemsListView.swift
+//  deau
+//
+//  Created by 濵田　悠樹 on 2024/11/26.
+//
+
+import SwiftUI
+
+struct MyItemsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyItemsListView()
+}
---
+++ deau/deauApp.swift
@@ -0,0 +1,17 @@
+//
+//  deauApp.swift
+//  deau
+//
+//  Created by 濵田　悠樹 on 2024/11/26.
+//
+
+import SwiftUI
+
+@main
+struct deauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
