0f8b60964d49ce92c32402a135fb88192fef0979
hamadayuuki <yuukirinnma@gmail.com>
2024-11-26 23:23:37 +0900

    Add packages for google calendar api

--- deau/Domain/Google/GoogleCalendar.swift
+++ deau/Domain/Google/GoogleCalendar.swift
@@ -5,3 +5,7 @@
 //  Created by yuki.hamada on 2024/11/26.
 //
 
+// クライアントID : 950372420525-9fntlq6pkblemr0q1ffg3jv2kjrmepg5.apps.googleusercontent.com
+// URLスキーム : com.googleusercontent.apps.950372420525-9fntlq6pkblemr0q1ffg3jv2kjrmepg5
+
+
